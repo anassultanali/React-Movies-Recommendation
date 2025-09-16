3e9fa96da72c3ae0590a0730bcddc36ad8742fe6
anassultanali <anassultanali@gmail.com>
2025-09-16 13:32:23 +0300

    49:10

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,9 +35,12 @@ function Home() {
         </form>
 
         <div className="movies-grid">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
+          {movies.map(
+            (movie) =>
+              movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id} />
+              )
+          )}
         </div>
       </div>
     </>
--- src/pages/favorite.jsx
+++ src/pages/favorite.jsx
@@ -2,7 +2,10 @@
 function Favorite() {
     return(
         <>
-        
+        <div className="favorites-empty">
+            <h2>No Favorite Movies Yet</h2>
+            <p>Add movies to your favorite.</p>
+        </div>
         </>
     )
 }
