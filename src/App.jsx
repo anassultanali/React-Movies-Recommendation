d933a0c19da8995554c746f5bd048e856c8f6426
Anas Sultan <107485610+anassultanali@users.noreply.github.com>
2025-10-23 19:36:18 +0300

    Update App.jsx
    the come back

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
       <Navbar />
       <main className="main-content">
         <Routes>
+
+          
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
